241d502fad79e030a55f523a2c395b8c1c98d685
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-22 23:24:31 +0200

    Fixed changeset comment in changelog-create-booking_system-schema.sql

--- src/main/resources/db/changelog/0.0.1/changelog-create-booking_system-schema.sql
+++ src/main/resources/db/changelog/0.0.1/changelog-create-booking_system-schema.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
 --changeset MelnykVL:create-booking_system-schema
---comment create new booking_system schema
+--comment create schema booking_system
 CREATE SCHEMA booking_system;
 --rollback drop schema booking_system;
