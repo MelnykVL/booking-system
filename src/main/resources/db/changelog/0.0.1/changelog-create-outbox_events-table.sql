630a21946f5db3b3ed6997d25a8c907ab5e27e69
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 00:19:56 +0200

    Created changelog-create-outbox_events-table.sql

---
+++ src/main/resources/db/changelog/0.0.1/changelog-create-outbox_events-table.sql
@@ -0,0 +1,19 @@
+--liquibase formatted sql
+
+--changeset MelnykVL:create-events-table
+--comment create new booking_system.events table
+CREATE TABLE events_log (
+    uuid            UUID PRIMARY KEY,
+    entity          VARCHAR(100) NOT NULL,
+    entity_id       BIGIN NOT NULL,
+    action          TEXT NOT NULL,
+    payload         JSONB NOT NULL,
+    created_at      TIMESTAMPTZ NOT NULL
+);
+--rollback drop table booking_system.events;
+
+--changeset MelnykVL:create-index-idx_events_entity_id
+--comment create index idx_events_entity_id for events_log.entity and events_log.entity_id columns
+CREATE INDEX idx_events_entity_id
+    ON events_log(entity, entity_id);
+--rollback drop index idx_events_entity_id
