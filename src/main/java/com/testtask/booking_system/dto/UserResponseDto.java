97e6e7172c0702fec49b8d06a01e436f385433f6
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 01:30:31 +0200

    Created UserView instead of UserResponseDto.java

--- src/main/java/com/testtask/booking_system/dto/UserResponseDto.java
+++
@@ -1,5 +0,0 @@
-package com.testtask.booking_system.dto;
-
-public record UserResponseDto(String firstName,
-                              String lastName,
-                              String email) {}
---
+++ src/main/java/com/testtask/booking_system/dto/view/UserView.java
@@ -0,0 +1,8 @@
+package com.testtask.booking_system.dto.view;
+
+public interface UserView {
+
+  String getFirstName();
+  String getLastName();
+  String getEmail();
+}
