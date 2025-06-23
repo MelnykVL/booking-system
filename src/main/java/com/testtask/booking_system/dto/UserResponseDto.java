1ca9466e216f0cd467083cac69591fe558ca2b99
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 01:22:39 +0200

    Created UserResponseDto

---
+++ src/main/java/com/testtask/booking_system/dto/UserResponseDto.java
@@ -0,0 +1,5 @@
+package com.testtask.booking_system.dto;
+
+public record UserResponseDto(String firstName,
+                              String lastName,
+                              String email) {}
