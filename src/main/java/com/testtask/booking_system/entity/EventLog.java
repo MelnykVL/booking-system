b92f192dd61b0ad436ede7e7714ad27c875e241b
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 00:22:16 +0200

    Created EventLog.java

---
+++ src/main/java/com/testtask/booking_system/entity/EventLog.java
@@ -0,0 +1,40 @@
+package com.testtask.booking_system.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Immutable;
+
+@Entity
+@Immutable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventLog {
+
+  @Id
+  @GeneratedValue
+  private UUID uuid;
+
+  @Column(nullable = false)
+  private String entity;
+
+  @Column(nullable = false)
+  private Long entityId;
+
+  @Column(nullable = false)
+  private String action;
+
+  @Column(nullable = false)
+  private String payload;
+
+  @CreationTimestamp
+  private Instant createdAt;
+}
