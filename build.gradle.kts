0e9aa98e2961a31fadc57bb0bab25eca1abe22c8
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 01:37:15 +0200

    Added mapstruct dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	java
-	alias(libs.plugins.spring.boot)
-	alias(libs.plugins.dependency.management)
+  java
+  alias(libs.plugins.spring.boot)
+  alias(libs.plugins.dependency.management)
 }
 
 group = "com.testtask"
@@ -10,37 +10,39 @@ version = "0.0.1"
 val javaVersion = providers.gradleProperty("javaVersion").map(String::toInt)
 
 java {
-	toolchain {
-		languageVersion = javaVersion.map(JavaLanguageVersion::of)
-		vendor = JvmVendorSpec.ADOPTIUM
-	}
+  toolchain {
+    languageVersion = javaVersion.map(JavaLanguageVersion::of)
+    vendor = JvmVendorSpec.ADOPTIUM
+  }
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.liquibase:liquibase-core")
-	implementation(libs.spring.doc.openapi)
-	runtimeOnly("org.postgresql:postgresql")
-	annotationProcessor(libs.lombok)
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:postgresql")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-data-redis")
+  implementation("org.liquibase:liquibase-core")
+  implementation(libs.spring.doc.openapi)
+  implementation(libs.mapstruct)
+  annotationProcessor(libs.lombok)
+  annotationProcessor(libs.mapstruct.processor)
+  runtimeOnly("org.postgresql:postgresql")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.boot:spring-boot-testcontainers")
+  testImplementation("org.testcontainers:junit-jupiter")
+  testImplementation("org.testcontainers:postgresql")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+  useJUnitPlatform()
 }
