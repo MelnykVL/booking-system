61cebf686567ebcad3de3a66123a5f6b59573795
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 05:15:38 +0200

    Created UnitResponseDto

---
+++ src/main/java/com/testtask/booking_system/dto/UnitResponseDto.java
@@ -0,0 +1,15 @@
+package com.testtask.booking_system.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.testtask.booking_system.enums.AccommodationType;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record UnitResponseDto(Long id,
+                              Long owner_id,
+                              Integer numberOfRooms,
+                              Integer floor,
+                              AccommodationType type,
+                              BigDecimal pricePerNight,
+                              @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC") Instant createdAt) {
+}
