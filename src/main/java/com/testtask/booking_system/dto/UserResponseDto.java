1f6b8ff88ae7dc7a919d5f6f94f2aff7ca23d558
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 05:10:56 +0200

    Added @JsonFormat with time pattern for createdAt field in UserResponseDto

--- src/main/java/com/testtask/booking_system/dto/UserResponseDto.java
+++ src/main/java/com/testtask/booking_system/dto/UserResponseDto.java
@@ -1,6 +1,10 @@
 package com.testtask.booking_system.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.Instant;
+
 public record UserResponseDto(Long id,
                               String firstName,
                               String lastName,
-                              String email) {}
+                              String email,
+                              @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC") Instant createdAt) {}
