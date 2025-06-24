fe86a8a816406eaa5515ea1cb350e827857b384d
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 04:53:27 +0200

    Created UnitCreateDto

---
+++ src/main/java/com/testtask/booking_system/dto/UnitCreateDto.java
@@ -0,0 +1,11 @@
+package com.testtask.booking_system.dto;
+
+import com.testtask.booking_system.enums.AccommodationType;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public record UnitCreateDto(@NotNull Long owner_id,
+                            @NotNull Integer numberOfRooms,
+                            @NotNull Integer floor,
+                            @NotNull AccommodationType type,
+                            @NotNull BigDecimal pricePerNight) {}
