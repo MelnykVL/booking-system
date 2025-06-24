4591be64e829f9a59d5200dd41b689317e4406dd
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 15:43:03 +0200

    Added @Pattern annotation to email field in UserPatchDto

--- src/main/java/com/testtask/booking_system/dto/UserPatchDto.java
+++ src/main/java/com/testtask/booking_system/dto/UserPatchDto.java
@@ -6,5 +6,6 @@ import jakarta.validation.constraints.Pattern;
 public record UserPatchDto(
     @Pattern(regexp = ".*\\S.*", message = "'firstName' cannot be whitespace only if specified.") String firstName,
     @Pattern(regexp = ".*\\S.*", message = "'lastName' cannot be whitespace only if specified.") String lastName,
+    @Pattern(regexp = ".*\\S.*", message = "'email' cannot be whitespace only if specified.")
     @Email(message = "'email' must be in email format.") String email
 ) {}
