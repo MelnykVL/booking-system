9e998649ddb14023fbdd69411d47d503c34f04cf
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 04:26:04 +0200

    Created UserEmailAlreadyExistsException.java

---
+++ src/main/java/com/testtask/booking_system/UserEmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.testtask.booking_system;
+
+public class UserEmailAlreadyExistsException extends RuntimeException {
+
+  public UserEmailAlreadyExistsException(String userEmail) {
+    super(String.format("User with email='%s' already exists", userEmail));
+  }
+}
