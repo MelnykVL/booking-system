1a55b4ed559aec80251cc22d8ebd88893aa529a3
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 01:57:21 +0200

    Renamed EventLogRepository to AuditRepository

--- src/main/java/com/testtask/booking_system/repository/AuditRepository.java
+++ src/main/java/com/testtask/booking_system/repository/AuditRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventLogRepository extends JpaRepository<EventLog, UUID> {
+public interface AuditRepository extends JpaRepository<EventLog, UUID> {
 }
