bed3f7c7f4fecb923df3a97a37f2a99bff5e969e
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 21:13:04 +0200

    Added applyMarkup method to MarkupUtils and changed markupFactor method

--- src/main/java/com/testtask/booking_system/util/MarkupUtils.java
+++ src/main/java/com/testtask/booking_system/util/MarkupUtils.java
@@ -7,13 +7,13 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class MarkupUtils {
 
+  public BigDecimal applyMarkup(BigDecimal price, BigDecimal markupPercent) {
+    return price.multiply(markupFactor(markupPercent))
+        .setScale(2, RoundingMode.HALF_UP);
+  }
+
   public BigDecimal markupFactor(BigDecimal markup) {
     return BigDecimal.ONE.add(
         markup.divide(BigDecimal.valueOf(100)));
-  }
-
-  public BigDecimal applyMarkup(BigDecimal price, BigDecimal markupFactor) {
-    return price.multiply(markupFactor)
-        .setScale(2, RoundingMode.HALF_UP);
   }
 }
