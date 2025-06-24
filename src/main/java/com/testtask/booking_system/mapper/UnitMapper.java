c80428d6a24b151a2441ae7c01f035ee7b390928
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 21:14:06 +0200

    Changed UnitMapper from interface to abstract and added bean injection and default applyMarkup method

--- src/main/java/com/testtask/booking_system/mapper/UnitMapper.java
+++ src/main/java/com/testtask/booking_system/mapper/UnitMapper.java
@@ -4,21 +4,35 @@ import com.testtask.booking_system.dto.UnitCreateDto;
 import com.testtask.booking_system.dto.UnitPatchDto;
 import com.testtask.booking_system.dto.UnitResponseDto;
 import com.testtask.booking_system.entity.Unit;
+import com.testtask.booking_system.props.PricingProps;
+import com.testtask.booking_system.util.MarkupUtils;
+import java.math.BigDecimal;
+import lombok.Setter;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public interface UnitMapper {
+@Setter(onMethod_ = {@Autowired})
+public abstract class UnitMapper {
 
-  Unit unitCreateDtoToUnit(UnitCreateDto unitCreateDto);
+  protected PricingProps pricingProps;
+
+  public abstract Unit unitCreateDtoToUnit(UnitCreateDto unitCreateDto);
 
   @Mapping(source = "owner.id", target = "ownerId")
-  UnitResponseDto unitToUnitResponseDto(Unit unit);
+  @Mapping(target = "pricePerNight",
+      expression = "java(applyMarkup(unit.getPricePerNight(), pricingProps.getMarkupPercent()))")
+  public abstract UnitResponseDto unitToUnitResponseDto(Unit unit);
 
   @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-  Unit patchUnit(UnitPatchDto unitPatchDto, @MappingTarget Unit unit);
+  public abstract Unit patchUnit(UnitPatchDto unitPatchDto, @MappingTarget Unit unit);
+
+  public BigDecimal applyMarkup(BigDecimal price, BigDecimal markupPercent) {
+    return MarkupUtils.applyMarkup(price, markupPercent);
+  }
 }
