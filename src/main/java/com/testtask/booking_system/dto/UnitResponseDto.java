2c06716a54b595b7e433cecf149d3c0886779704
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 05:30:41 +0200

    Removed @JsonFormat from UserResponseDto and UnitResponseDto

--- src/main/java/com/testtask/booking_system/dto/UnitResponseDto.java
+++ src/main/java/com/testtask/booking_system/dto/UnitResponseDto.java
@@ -1,6 +1,5 @@
 package com.testtask.booking_system.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.testtask.booking_system.enums.AccommodationType;
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -11,5 +10,5 @@ public record UnitResponseDto(Long id,
                               Integer floor,
                               AccommodationType type,
                               BigDecimal pricePerNight,
-                              @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC") Instant createdAt) {
+                              Instant createdAt) {
 }
--- src/main/java/com/testtask/booking_system/dto/UserResponseDto.java
+++ src/main/java/com/testtask/booking_system/dto/UserResponseDto.java
@@ -1,10 +1,9 @@
 package com.testtask.booking_system.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.Instant;
 
 public record UserResponseDto(Long id,
                               String firstName,
                               String lastName,
                               String email,
-                              @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC") Instant createdAt) {}
+                              Instant createdAt) {}
