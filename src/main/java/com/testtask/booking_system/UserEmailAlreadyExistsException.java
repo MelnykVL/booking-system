9875506cae3319d81b3f4d298e8580502050a752
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 04:43:33 +0200

    Fixed error message in UserEmailAlreadyExistsException

--- src/main/java/com/testtask/booking_system/UserEmailAlreadyExistsException.java
+++ src/main/java/com/testtask/booking_system/UserEmailAlreadyExistsException.java
@@ -3,6 +3,6 @@ package com.testtask.booking_system;
 public class UserEmailAlreadyExistsException extends RuntimeException {
 
   public UserEmailAlreadyExistsException(String userEmail) {
-    super(String.format("User with email='%s' already exists", userEmail));
+    super(String.format("User with email '%s' already exists", userEmail));
   }
 }
