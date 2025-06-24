a12e5bf1d68be0b128e5e4f6e5006cd484c47c01
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 05:34:47 +0200

    Fixed field names in UnitCreateDto

--- src/main/java/com/testtask/booking_system/dto/UnitCreateDto.java
+++ src/main/java/com/testtask/booking_system/dto/UnitCreateDto.java
@@ -4,7 +4,7 @@ import com.testtask.booking_system.enums.AccommodationType;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-public record UnitCreateDto(@NotNull Long owner_id,
+public record UnitCreateDto(@NotNull Long ownerId,
                             @NotNull Integer numberOfRooms,
                             @NotNull Integer floor,
                             @NotNull AccommodationType type,
