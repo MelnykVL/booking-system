cd4e070add7b5d62a4551f7ab2b2630838498d82
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 21:13:22 +0200

    Added @Getter to PricingProps

--- src/main/java/com/testtask/booking_system/props/PricingProps.java
+++ src/main/java/com/testtask/booking_system/props/PricingProps.java
@@ -3,12 +3,14 @@ package com.testtask.booking_system.props;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import java.math.BigDecimal;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "booking-system.pricing")
 @Validated
+@Getter
 @RequiredArgsConstructor
 public class PricingProps {
 
