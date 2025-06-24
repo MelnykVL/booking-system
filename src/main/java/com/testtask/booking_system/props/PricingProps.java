402275e966d91ecaea8b521b43b2d8234801edbd
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 20:20:08 +0200

    Created PricingProps

---
+++ src/main/java/com/testtask/booking_system/props/PricingProps.java
@@ -0,0 +1,18 @@
+package com.testtask.booking_system.props;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@ConfigurationProperties(prefix = "booking-system.pricing")
+@Validated
+@RequiredArgsConstructor
+public class PricingProps {
+
+  @DecimalMin("0")
+  @DecimalMax("100")
+  private final BigDecimal markupPercent;
+}
