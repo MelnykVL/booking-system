0c20e2f9209cd10fc592f5df444a7801f33843d4
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 17:31:57 +0200

    Added patchUnit method to UnitMapper

--- src/main/java/com/testtask/booking_system/mapper/UnitMapper.java
+++ src/main/java/com/testtask/booking_system/mapper/UnitMapper.java
@@ -1,11 +1,15 @@
 package com.testtask.booking_system.mapper;
 
 import com.testtask.booking_system.dto.UnitCreateDto;
+import com.testtask.booking_system.dto.UnitPatchDto;
 import com.testtask.booking_system.dto.UnitResponseDto;
 import com.testtask.booking_system.entity.Unit;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UnitMapper {
@@ -14,4 +18,7 @@ public interface UnitMapper {
 
   @Mapping(source = "owner.id", target = "ownerId")
   UnitResponseDto unitToUnitResponseDto(Unit unit);
+
+  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+  Unit patchUnit(UnitPatchDto unitPatchDto, @MappingTarget Unit unit);
 }
