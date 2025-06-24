d8e4a38f8ee5381ecb93438ea50ca4ca06f0b82f
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 17:53:29 +0200

    Added constructor with 1 parameter to ResourceNotFountException

--- src/main/java/com/testtask/booking_system/exception/ResourceNotFountException.java
+++ src/main/java/com/testtask/booking_system/exception/ResourceNotFountException.java
@@ -2,6 +2,10 @@ package com.testtask.booking_system.exception;
 
 public class ResourceNotFountException extends RuntimeException {
 
+  public ResourceNotFountException(String message) {
+    super(message);
+  }
+
   public ResourceNotFountException(String resourceName, long resourceId) {
     super(String.format("'%s' with id '%d' does not exist.", resourceName, resourceId));
   }
