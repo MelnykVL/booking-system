e89e1fa88643e8fcccdaf3368942aa90e6181785
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 05:31:29 +0200

    Created UnitMapper

---
+++ src/main/java/com/testtask/booking_system/mapper/UnitMapper.java
@@ -0,0 +1,17 @@
+package com.testtask.booking_system.mapper;
+
+import com.testtask.booking_system.dto.UnitCreateDto;
+import com.testtask.booking_system.dto.UnitResponseDto;
+import com.testtask.booking_system.entity.Unit;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UnitMapper {
+
+  Unit unitCreateDtoToUnit(UnitCreateDto unitCreateDto);
+
+  @Mapping(source = "owner.id", target = "ownerId")
+  UnitResponseDto unitToUnitResponseDto(Unit unit);
+}
