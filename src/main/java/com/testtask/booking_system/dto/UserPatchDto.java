90da6bd8d768a3df1516fbae3d360854df06fb6b
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 15:39:39 +0200

    Created UserPatchDto

---
+++ src/main/java/com/testtask/booking_system/dto/UserPatchDto.java
@@ -0,0 +1,10 @@
+package com.testtask.booking_system.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+public record UserPatchDto(
+    @Pattern(regexp = ".*\\S.*", message = "'firstName' cannot be whitespace only if specified.") String firstName,
+    @Pattern(regexp = ".*\\S.*", message = "'lastName' cannot be whitespace only if specified.") String lastName,
+    @Email(message = "'email' must be in email format.") String email
+) {}
