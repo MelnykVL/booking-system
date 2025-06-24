34e0bee3d02c51595e9e01af75dcfba80bf96795
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 04:45:25 +0200

    Implemented UserService and UserController

---
+++ src/main/java/com/testtask/booking_system/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.testtask.booking_system.controller;
+
+import com.testtask.booking_system.dto.UserCreateDto;
+import com.testtask.booking_system.dto.UserResponseDto;
+import com.testtask.booking_system.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+
+  private final UserService userService;
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<UserResponseDto> getUser(@PathVariable Long userId) {
+    return userService.getUser(userId);
+  }
+
+  @PostMapping
+  public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateDto userCreateDto) {
+    return userService.createUser(userCreateDto);
+  }
+}
--- src/main/java/com/testtask/booking_system/service/UserService.java
+++ src/main/java/com/testtask/booking_system/service/UserService.java
@@ -1,7 +1,40 @@
 package com.testtask.booking_system.service;
 
+import com.testtask.booking_system.UserEmailAlreadyExistsException;
+import com.testtask.booking_system.dto.UserCreateDto;
+import com.testtask.booking_system.dto.UserResponseDto;
+import com.testtask.booking_system.entity.User;
+import com.testtask.booking_system.mapper.UserMapper;
+import com.testtask.booking_system.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+  private final UserRepository userRepository;
+  private final UserMapper userMapper;
+
+  public ResponseEntity<UserResponseDto> getUser(Long userId) {
+    User user = userRepository.findById(userId).orElseThrow(() -> {
+      String errorMessage = String.format("User with id '%d' does not exist", userId);
+      return new EntityNotFoundException(errorMessage);
+    });
+    UserResponseDto userResponseDto = userMapper.userToUserResponseDto(user);
+
+    return ResponseEntity.ok(userResponseDto);
+  }
+
+  public ResponseEntity<UserResponseDto> createUser(UserCreateDto userCreateDto) {
+    if (userRepository.existsByEmail(userCreateDto.email())) {
+      throw new UserEmailAlreadyExistsException(userCreateDto.email());
+    }
+    User user = userMapper.userCreateDtoToUser(userCreateDto);
+    UserResponseDto userResponseDto = userMapper.userToUserResponseDto(userRepository.save(user));
+
+    return ResponseEntity.ok(userResponseDto);
+  }
 }
