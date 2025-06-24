525d6615b3650d41247accc9deed2f7c3e5a8224
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 21:14:39 +0200

    Added @EnableConfigurationProperties to BookingSystemApplication

--- src/main/java/com/testtask/booking_system/BookingSystemApplication.java
+++ src/main/java/com/testtask/booking_system/BookingSystemApplication.java
@@ -1,13 +1,15 @@
 package com.testtask.booking_system;
 
+import com.testtask.booking_system.props.PricingProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PricingProps.class)
 public class BookingSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookingSystemApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BookingSystemApplication.class, args);
+  }
 }
