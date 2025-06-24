a708b316579d8dfe006b9f4d3377b1eb9a0b631a
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-24 02:20:45 +0200

    Renamed events_log table to event_logs

--- src/main/resources/db/changelog/0.0.1/changelog-create-event_logs-table.sql
+++ src/main/resources/db/changelog/0.0.1/changelog-create-event_logs-table.sql
@@ -1,11 +1,11 @@
 --liquibase formatted sql
 
---changeset MelnykVL:create-events_log-table
---comment create new booking_system.events_log table
-CREATE TABLE events_log (
+--changeset MelnykVL:create-event_logs-table
+--comment create new booking_system.event_logs table
+CREATE TABLE event_logs (
     uuid            UUID PRIMARY KEY,
     entity          VARCHAR(100) NOT NULL,
-    entity_id       BIGIN NOT NULL,
+    entity_id       BIGINT NOT NULL,
     action          TEXT NOT NULL,
     payload         JSONB NOT NULL,
     created_at      TIMESTAMPTZ NOT NULL
@@ -13,7 +13,7 @@ CREATE TABLE events_log (
 --rollback drop table booking_system.events;
 
 --changeset MelnykVL:create-index-idx_events_entity_id
---comment create index idx_events_entity_id for events_log.entity and events_log.entity_id columns
+--comment create index idx_events_entity_id for event_logs.entity and event_logs.entity_id columns
 CREATE INDEX idx_events_entity_id
-    ON events_log(entity, entity_id);
+    ON event_logs(entity, entity_id);
 --rollback drop index idx_events_entity_id
