89c29460552504fd18ee02681dfcc2d1045a55b4
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 02:06:08 +0200

    Added countTotalCostForPeriod to MarkupUtils

--- src/main/java/com/testtask/booking_system/util/MarkupUtils.java
+++ src/main/java/com/testtask/booking_system/util/MarkupUtils.java
@@ -2,18 +2,31 @@ package com.testtask.booking_system.util;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class MarkupUtils {
 
+  private static final int MONEY_SCALE = 2;
+  private static final RoundingMode ROUNDING = RoundingMode.HALF_UP;
+
   public BigDecimal applyMarkup(BigDecimal price, BigDecimal markupPercent) {
     return price.multiply(markupFactor(markupPercent))
-        .setScale(2, RoundingMode.HALF_UP);
+        .setScale(MONEY_SCALE, ROUNDING);
   }
 
   public BigDecimal markupFactor(BigDecimal markup) {
     return BigDecimal.ONE.add(
         markup.divide(BigDecimal.valueOf(100)));
+  }
+
+  public BigDecimal countTotalCostForPeriod(LocalDate checkInOn, LocalDate checkOutOn, BigDecimal pricePerNight,
+      BigDecimal markupPercent) {
+    long nights = ChronoUnit.DAYS.between(checkInOn, checkOutOn);
+    BigDecimal net = pricePerNight.multiply(BigDecimal.valueOf(nights));
+
+    return applyMarkup(net, markupPercent);
   }
 }
