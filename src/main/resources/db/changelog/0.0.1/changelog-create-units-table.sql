a41b45fa8d051ef2c260fc7a66954bfe5c65eb9e
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 21:16:40 +0200

    Added partition by type field in changelog-create-units-table.sql

--- src/main/resources/db/changelog/0.0.1/changelog-create-units-table.sql
+++ src/main/resources/db/changelog/0.0.1/changelog-create-units-table.sql
@@ -1,9 +1,12 @@
 --liquibase formatted sql
 
---changeset MelnykVL:create-units-table
---comment create table booking_system.units
+--changeset MelnykVL:create-accommodation_type-enum
+--comment create enum accommodation_type
 CREATE TYPE ACCOMMODATION_TYPE AS ENUM('HOME', 'FLAT', 'APARTMENTS');
+--rollback drop enum accommodation_type
 
+--changeset MelnykVL:create-booking_system.units-table
+--comment create table booking_system.units
 CREATE TABLE booking_system.units (
     id                  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     owner_id            BIGINT REFERENCES booking_system.users(id),
@@ -14,7 +17,7 @@ CREATE TABLE booking_system.units (
     description         TEXT,
     created_at          TIMESTAMPTZ NOT NULL,
     updated_at          TIMESTAMPTZ NOT NULL
-);
+) PARTITION BY list(type);
 --rollback drop table booking_system.users
 
 --changeset MelnykVL:create-index-idx_unit_catalog
@@ -26,3 +29,21 @@ CREATE INDEX idx_unit_catalog ON booking_system.units(number_of_rooms, floor, ty
 --comment create index idx_unit_price
 CREATE INDEX idx_unit_price ON booking_system.units(price_per_night);
 --rollback drop index idx_unit_price
+
+--changeset MelnykVL:create-partition-booking_system.units-homes
+--comment create partition homes for booking_system.units table
+CREATE TABLE homes PARTITION OF booking_system.units
+    FOR VALUES IN ('HOME');
+--rollback drop partition homes
+
+--changeset MelnykVL:create-partition-booking_system.units-flats
+--comment create partition flats for booking_system.units table
+CREATE TABLE flats PARTITION OF booking_system.units
+    FOR VALUES IN ('FLAT');
+--rollback drop partition flats
+
+--changeset MelnykVL:create-partition-booking_system.units-apartments
+--comment create partition apartments for booking_system.units table
+CREATE TABLE apartments PARTITION OF booking_system.units
+    FOR VALUES IN ('APARTMENTS');
+--rollback drop partition apartments
