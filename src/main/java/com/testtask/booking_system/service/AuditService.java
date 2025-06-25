f2656a5b41d6473241aa03f567601e8589c48258
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 23:53:22 +0200

    Added logBatch methods to AuditService

--- src/main/java/com/testtask/booking_system/service/AuditService.java
+++ src/main/java/com/testtask/booking_system/service/AuditService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testtask.booking_system.entity.EventLog;
 import com.testtask.booking_system.exception.ObjectToJsonProcessingException;
 import com.testtask.booking_system.repository.AuditRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,15 +18,28 @@ public class AuditService {
   private final AuditRepository auditRepository;
   private final ObjectMapper objectMapper;
 
-  public void log(Class<?> entity, String action) {
-    this.log(entity, null, action, null);
-  }
-
   public void log(Class<?> entity, String action, Object payload) {
     this.log(entity, null, action, payload);
   }
 
   public void log(Class<?> entity, Long entityId, String action, Object payload) {
+    EventLog eventLog = createEventLog(entity, entityId, action, payload);
+    auditRepository.save(eventLog);
+  }
+
+  public void logBatch(Class<?> entity, String action, List<Object> payloads) {
+    this.logBatch(entity, null, action, payloads);
+  }
+
+  public void logBatch(Class<?> entity, Long entityId, String action, List<Object> payloads) {
+    List<EventLog> eventLogs =
+        payloads.stream()
+                .map(p -> createEventLog(entity, entityId, action, p))
+                .toList();
+    auditRepository.saveAll(eventLogs);
+  }
+
+  private EventLog createEventLog(Class<?> entity, Long entityId, String action, Object payload) {
     EventLog eventLog = new EventLog();
     eventLog.setEntity(entity.getSimpleName());
     eventLog.setEntityId(entityId);
@@ -35,6 +49,6 @@ public class AuditService {
     } catch (JsonProcessingException ex) {
       throw new ObjectToJsonProcessingException(ex);
     }
-    auditRepository.save(eventLog);
+    return eventLog;
   }
 }
