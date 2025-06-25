45c97819407c2fa210e0bf8256c0a90985a3a173
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 05:21:02 +0200

    Added isDatesMatch method to BookingCreateDto

--- src/main/java/com/testtask/booking_system/dto/BookingCreateDto.java
+++ src/main/java/com/testtask/booking_system/dto/BookingCreateDto.java
@@ -1,10 +1,22 @@
 package com.testtask.booking_system.dto;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.Objects;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public record BookingCreateDto(
+    @NotNull(message = "'checkInOn' cannot be null.") @DateTimeFormat(pattern = "yyyy-MM-dd")
     @FutureOrPresent LocalDate checkInOn,
+    @NotNull(message = "'checkOutOn' cannot be null.") @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Future LocalDate checkOutOn
-) {}
+) {
+
+  @AssertTrue(message = "The dates cannot be the same.")
+  private boolean isDatesMatch() {
+    return Objects.nonNull(checkInOn) && Objects.nonNull(checkOutOn) && checkOutOn.isAfter(checkInOn);
+  }
+}
