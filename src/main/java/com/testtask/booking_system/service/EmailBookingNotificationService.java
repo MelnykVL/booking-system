5aaca66e9570676bdd274db98c1d944d9fc830e4
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 23:54:35 +0200

    Renamed BookingNotificationService to EmailBookingNotificationService

--- src/main/java/com/testtask/booking_system/service/EmailBookingNotificationService.java
+++ src/main/java/com/testtask/booking_system/service/EmailBookingNotificationService.java
@@ -1,5 +1,6 @@
 package com.testtask.booking_system.service;
 
+import com.testtask.booking_system.NotificationSender;
 import com.testtask.booking_system.dto.notification.BookingNotificationDto;
 import com.testtask.booking_system.enums.EventLogAction;
 import lombok.RequiredArgsConstructor;
@@ -11,16 +12,28 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class BookingNotificationService {
+public class EmailBookingNotificationService implements NotificationService {
 
   private static final String DEFAULT_MESSAGE_CANCEL_BOOKING = "Your booking '%d' has been cancelled.";
 
+  private final UserService userService;
   private final AuditService auditService;
+  private NotificationSender notificationSender;
 
   @Async
   @TransactionalEventListener
   public void sendNotification(BookingNotificationDto bookingNotificationDto) {
+    String toUser = userService.findUser(bookingNotificationDto.getUserId()).getBody().email();
+    bookingNotificationDto.setContent(DEFAULT_MESSAGE_CANCEL_BOOKING);
+    notificationSender.send(toUser, DEFAULT_MESSAGE_CANCEL_BOOKING);
     auditService.log(NotificationService.class, EventLogAction.SEND_NOTIFICATION.name(), bookingNotificationDto);
-    log.info("EMULATING NOTIFICATION...");
+    for (int i = 0; i < 100; i++) {
+      log.info("EMULATING NOTIFICATION...");
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
