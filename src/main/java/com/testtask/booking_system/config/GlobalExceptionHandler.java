2d6bd60b42a9da0d587b81633ae3a4b24d17777e
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 03:06:14 +0200

    Added NoResourceFoundException to GlobalExceptionHandler

--- src/main/java/com/testtask/booking_system/config/GlobalExceptionHandler.java
+++ src/main/java/com/testtask/booking_system/config/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,6 +16,11 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> handleRuntimeErrors(Exception ex) {
     return new ResponseEntity<>("Bad request", HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(NoResourceFoundException.class)
+  public ResponseEntity<String> handleNoResourceFoundException(NoResourceFoundException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
