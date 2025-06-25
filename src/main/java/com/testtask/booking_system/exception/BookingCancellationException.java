169637baa14b53e268995c325235ef504f9cdb3b
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 03:59:05 +0200

    Removed BookingCancellationException.java

--- src/main/java/com/testtask/booking_system/exception/BookingCancellationException.java
+++
@@ -1,8 +0,0 @@
-package com.testtask.booking_system.exception;
-
-public class BookingCancellationException extends RuntimeException {
-
-  public BookingCancellationException(String message) {
-    super(message);
-  }
-}
