592221ecae1ff3adc35040ca552608a252eddbf9
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 03:01:48 +0200

    Fixed error message in ResourceNotFountException

--- src/main/java/com/testtask/booking_system/exception/ResourceNotFountException.java
+++ src/main/java/com/testtask/booking_system/exception/ResourceNotFountException.java
@@ -7,6 +7,6 @@ public class ResourceNotFountException extends RuntimeException {
   }
 
   public ResourceNotFountException(String resourceName, long resourceId) {
-    super(String.format("'%s' with id '%d' does not exist.", resourceName, resourceId));
+    super(String.format("%s with id '%d' does not exist.", resourceName, resourceId));
   }
 }
