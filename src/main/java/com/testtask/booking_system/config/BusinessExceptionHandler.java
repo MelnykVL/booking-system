8835528570292c6d48433e0931818cc82f08477e
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 04:00:52 +0200

    Added UserNotOwnerBookingException and UnitNotBookedByUserException to BusinessExceptionHandler

--- src/main/java/com/testtask/booking_system/config/BusinessExceptionHandler.java
+++ src/main/java/com/testtask/booking_system/config/BusinessExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.testtask.booking_system.config;
 
 import com.testtask.booking_system.exception.ResourceNotFountException;
+import com.testtask.booking_system.exception.UnitNotBookedByUserException;
 import com.testtask.booking_system.exception.UserEmailAlreadyExistsException;
+import com.testtask.booking_system.exception.UserNotOwnerBookingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,6 +19,16 @@ public class BusinessExceptionHandler {
 
   @ExceptionHandler(ResourceNotFountException.class)
   public ResponseEntity<String> handleResourceNotFountException(ResourceNotFountException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(UserNotOwnerBookingException.class)
+  public ResponseEntity<String> handleUserNotOwnerBookingException(UserNotOwnerBookingException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+  }
+
+  @ExceptionHandler(UnitNotBookedByUserException.class)
+  public ResponseEntity<String> handleUnitNotBookedByUserException(UnitNotBookedByUserException ex) {
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
   }
 }
