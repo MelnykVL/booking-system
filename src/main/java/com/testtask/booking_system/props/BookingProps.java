134c2059b5342c311a04caee45ea0e0a2608c896
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 18:25:32 +0200

    Replaced @FutureOrPresent with @DurationMin annotation for expirationTime in BookingProps

--- src/main/java/com/testtask/booking_system/props/BookingProps.java
+++ src/main/java/com/testtask/booking_system/props/BookingProps.java
@@ -1,8 +1,8 @@
 package com.testtask.booking_system.props;
 
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Positive;
 import java.time.Duration;
+import org.hibernate.validator.constraints.time.DurationMin;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +10,6 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "booking-system.booking")
 @Validated
 public record BookingProps(
-    @DefaultValue("15m") @FutureOrPresent Duration expirationTime,
+    @DefaultValue("15m") @DurationMin(minutes = 5) Duration expirationTime,
     @DefaultValue("365") @Positive Integer maxBookingTerm
 ) {}
