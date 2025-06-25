13ccd4fa0f92f9692cc8f30de30afa4d1c940d01
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 05:25:27 +0200

    Added @EnableScheduling to BookingSystemApplication

--- src/main/java/com/testtask/booking_system/BookingSystemApplication.java
+++ src/main/java/com/testtask/booking_system/BookingSystemApplication.java
@@ -4,8 +4,10 @@ import com.testtask.booking_system.props.PricingProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties(PricingProps.class)
 public class BookingSystemApplication {
 
