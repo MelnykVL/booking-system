dadaf195949dc34080d0224ea3bb0793632697bd
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-25 03:02:04 +0200

    Added HttpRequestMethodNotSupportedException to GlobalExceptionHandler

--- src/main/java/com/testtask/booking_system/config/GlobalExceptionHandler.java
+++ src/main/java/com/testtask/booking_system/config/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.stream.Collectors;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +15,12 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> handleRuntimeErrors(Exception ex) {
     return new ResponseEntity<>("Bad request", HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  public ResponseEntity<String> handleHttpRequestMethodNotSupportedException(
+      HttpRequestMethodNotSupportedException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.METHOD_NOT_ALLOWED);
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
