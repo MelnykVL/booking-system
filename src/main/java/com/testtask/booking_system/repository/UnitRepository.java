51c86a0fe41ad35068d7afa08861cebf16643a98
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-26 04:14:08 +0200

    Extended UnitRepository from JpaSpecificationExecutor

--- src/main/java/com/testtask/booking_system/repository/UnitRepository.java
+++ src/main/java/com/testtask/booking_system/repository/UnitRepository.java
@@ -4,10 +4,11 @@ import com.testtask.booking_system.entity.Unit;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UnitRepository extends JpaRepository<Unit, Long> {
+public interface UnitRepository extends JpaRepository<Unit, Long>, JpaSpecificationExecutor<Unit> {
 
   Page<Unit> findAllByOwnerId(Long ownerId, Pageable pageable);
 }
