0c75edb693031af3c4cb6983d42ca21c54f10097
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-26 06:18:07 +0200

    Created RedisConfig

---
+++ src/main/java/com/testtask/booking_system/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.testtask.booking_system.config;
+
+import com.testtask.booking_system.dto.response.UnitResponseDto;
+import java.time.Duration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@Configuration
+public class RedisConfig {
+
+  @Bean
+  public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+    RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofMinutes(3))
+        .disableCachingNullValues()
+        .serializeValuesWith(RedisSerializationContext.SerializationPair
+            .fromSerializer(new Jackson2JsonRedisSerializer<>(UnitResponseDto.class)));
+
+    return RedisCacheManager.builder(redisConnectionFactory)
+        .cacheDefaults(cacheConfig)
+        .build();
+  }
+}
