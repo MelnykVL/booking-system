81c610efb059696f41274ded49c566eabba8087f
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-26 10:33:10 +0200

    Created PaymentRequestDto

---
+++ src/main/java/com/testtask/booking_system/dto/request/PaymentRequestDto.java
@@ -0,0 +1,10 @@
+package com.testtask.booking_system.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+public record PaymentRequestDto(
+    @NotNull(message = "'amount' cannot be null.")
+    @Positive(message = "'amount' must be greater than 0.00.") BigDecimal amount
+) {}
