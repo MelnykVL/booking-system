a0b1827d441213a810940ab91567bbb59aac55e1
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-26 09:13:56 +0200

    Created OpenApiConfig

---
+++ src/main/java/com/testtask/booking_system/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.testtask.booking_system.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Booking System API")
+            .version("1.0.0")
+            .description("""
+                API for real estate booking system.
+                Allows you to manage reservations, create manage them and process payments.
+                """)
+            .contact(new Contact()
+                .name("Vladyslav Melnyk")
+                .url("https://github.com/MelnykVL/booking-system")));
+  }
+}
