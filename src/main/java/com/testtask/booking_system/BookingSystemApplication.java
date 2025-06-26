ccd1ba1fd24b49ddeffda7b6af0849c10d88f1f7
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-26 06:06:11 +0200

    Added @EnableCaching to main class

--- src/main/java/com/testtask/booking_system/BookingSystemApplication.java
+++ src/main/java/com/testtask/booking_system/BookingSystemApplication.java
@@ -5,10 +5,12 @@ import com.testtask.booking_system.props.PricingProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 @EnableConfigurationProperties({PricingProps.class, BookingProps.class})
 public class BookingSystemApplication {
 
