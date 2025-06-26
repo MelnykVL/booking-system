c0099f0d852fe03f7931f59530ed94bbad6232f5
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-06-26 03:13:38 +0200

    Added @Min to UnitCreateDto

--- src/main/java/com/testtask/booking_system/dto/request/UnitCreateDto.java
+++ src/main/java/com/testtask/booking_system/dto/request/UnitCreateDto.java
@@ -1,6 +1,7 @@
 package com.testtask.booking_system.dto.request;
 
 import com.testtask.booking_system.enums.AccommodationType;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
@@ -8,7 +9,8 @@ import java.math.BigDecimal;
 public record UnitCreateDto(
     @NotNull(message = "'numberOfRooms' cannot be null.")
     @Positive(message = "'numberOfRooms' must be greater than 0.") Integer numberOfRooms,
-    @NotNull(message = "'floor' cannot be null.") Integer floor,
+    @NotNull(message = "'floor' cannot be null.")
+    @Min(value = 0, message = "'floor' cannot be less than 0.") Integer floor,
     @NotNull(message = "'type' cannot be blank.") AccommodationType type,
     @NotNull(message = "'pricePerNight' cannot be null.")
     @Positive(message = "'pricePerNight' must be greater than 0.00.") BigDecimal pricePerNight,
